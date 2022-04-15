7b929b5259bdcd6ebd2bca04398c72c4f040e396
Rasmus Wissing Kallehauge <Hannimis@live.dk>
2022-04-15 18:46:56 +0200

    fix column names in user, post and reactio

--- app/src/main/java/com/example/databaseproject/Post.java
+++ app/src/main/java/com/example/databaseproject/Post.java
@@ -1,13 +1,13 @@
 package com.example.databaseproject;
 import androidx.room.*;
 
-@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "name", childColumns = "username"))
+@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "user_id"))
 public class Post {
 
     @PrimaryKey
     public int id;
 
-    public String username;
+    public String user_id;
     public String content;
     public Integer stamp;
 
