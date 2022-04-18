363b40dbfbeae4df6429c88151a8148e9ecc482f
Rasmus Wissing Kallehauge <Hannimis@live.dk>
2022-04-18 16:55:10 +0200

    temporary fix to post <-> thread problem

--- app/src/main/java/com/example/databaseproject/postCreation.java
+++ app/src/main/java/com/example/databaseproject/postCreation.java
@@ -41,46 +41,8 @@ public class postCreation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_creation);
         List<Post> l  = getPosts();
-        LinearLayout layout = findViewById(R.id.postCreation);
         for(Post post:l){
-            RelativeLayout rLayout = new RelativeLayout(this);
-
-            LinearLayout linLayout = new LinearLayout(this);
-            linLayout.setOrientation(LinearLayout.VERTICAL);
-
-
-
-            GridLayout g = new GridLayout(this);
-
-            g.setColumnCount(2);
-            g.setRowCount(1);
-
-            TextView user = new TextView(this);
-            user.setText(post.user_id);
-            g.addView(user);
-
-            TextView stamp = new TextView(this);
-            stamp.setText(post.stamp);
-            g.addView(stamp);
-
-            linLayout.addView(g);
-
-            TextView content = new TextView(this);
-            content.setText(post.content);
-
-            linLayout.addView(content);
-
-            LinearLayout.LayoutParams lLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            lLayoutParams.setMargins(10,10,10,10);
-
-
-            rLayout.addView(linLayout, lLayoutParams);
-
-            RelativeLayout.LayoutParams rLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-            // TODO : This doesnt really center each post in feed, fix at a further date
-            rLayoutParams.alignWithParent = true;
-            layout.addView(rLayout);
+            makePost(post);
         }
     }
 
@@ -110,16 +72,17 @@ public class postCreation extends AppCompatActivity {
 
         SessionHandler sh = new SessionHandler(this,"user");
         String user_id = sh.getString("user_id");
-
-        new Thread(()->{
         String content = ((EditText) findViewById(R.id.userTextInput)).getText().toString();
-
         String epoch = OffsetDateTime.now().toString();
+
+        Post post = new Post(user_id,content,epoch);
+        makePost(post);
+        new Thread(()->{
+
 
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "User").fallbackToDestructiveMigration().build();
         PostDao postdao = db.PostDao();
-        Post post = new Post(user_id,content,epoch);
 
         List<Long> result = postdao.insertAll(post);
         System.out.println("post_id = " + result.get(0));
@@ -146,6 +109,50 @@ public class postCreation extends AppCompatActivity {
         PostDao postdao = db.PostDao();
         List<Post> posts = postdao.getAll();
         return posts;
+    }
+
+    public void makePost(Post post){
+
+        LinearLayout layout = findViewById(R.id.postCreation);
+
+        RelativeLayout rLayout = new RelativeLayout(this);
+
+        LinearLayout linLayout = new LinearLayout(this);
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+
+
+
+        GridLayout g = new GridLayout(this);
+
+        g.setColumnCount(2);
+        g.setRowCount(1);
+
+        TextView user = new TextView(this);
+        user.setText(post.user_id);
+        g.addView(user);
+
+        TextView stamp = new TextView(this);
+        stamp.setText(post.stamp);
+        g.addView(stamp);
+
+        linLayout.addView(g);
+
+        TextView content = new TextView(this);
+        content.setText(post.content);
+
+        linLayout.addView(content);
+
+        LinearLayout.LayoutParams lLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lLayoutParams.setMargins(10,10,10,10);
+
+
+        rLayout.addView(linLayout, lLayoutParams);
+
+        RelativeLayout.LayoutParams rLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        // TODO : This doesnt really center each post in feed, fix at a further date
+        rLayoutParams.alignWithParent = true;
+        layout.addView(rLayout);
     }
 
 }
